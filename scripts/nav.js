16590ce7074d6737df5c98bd856d94837a100b09
Eric Hu <eric.hu@infor.com>
2019-06-07 10:58:06 -0400

    first commit to git, incomplete website

---
+++ scripts/nav.js
@@ -0,0 +1,22 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click',() => {
+        //Display or hide opacity of the menu
+        var value = nav.style.left;
+        console.log("before: " + value);
+        if (!value || value == "-30%" || value < 0) {
+            nav.style.left = 0;
+        } else {
+            nav.style.left = "-30%";
+        }
+        console.log("after: " + value);
+        //burger animation
+        burger.classList.toggle('toggle');
+    });
+
+}
+
+
+navSlide();
